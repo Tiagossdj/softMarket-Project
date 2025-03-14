7d7d3bdc5df4402cc8ed278fd7b0f84648f7f750
Tiago Silvino <117856931+Tiagossdj@users.noreply.github.com>
2025-03-14 17:57:46 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123456@localhost:5432/softmarket" # <123456> exemplo de senha!
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123456@localhost:5432/softmarket" # <postgres> = usuario e <123456> exemplo de senha!
     SQLALCHEMY_TRACK_MODIFICATIONS = False
